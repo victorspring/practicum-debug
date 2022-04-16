5e255306814c1064965277d2cb9a888c015a4be8
Victor <victorbodrovit@gmail.com>
2022-04-16 18:09:12 +0500

    Initial commit

--- src/ru/yandex/practicum/debug/Main.java
+++ src/ru/yandex/practicum/debug/Main.java
@@ -1,10 +1,14 @@
 package ru.yandex.practicum.debug;
 
 public class Main {
+
+    //Demo
+
     static int N = 1;
 
     public static void main(String[] args) {
         for (int i = -5; i <= 5; i++) {
+            System.out.println(Math.sin(i));
             System.out.println(1 / i);
         }
     }
