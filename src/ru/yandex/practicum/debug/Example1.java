528dec792742d036cb8aeb88e48286dc4f0156ce
Victor <viktorb94@yandex.ru>
2022-07-09 17:46:10 +0500

    Update example

--- src/ru/yandex/practicum/debug/Example1.java
+++ src/ru/yandex/practicum/debug/Example1.java
@@ -3,7 +3,13 @@ package ru.yandex.practicum.debug;
 public class Example1 {
 
     public static void main(String[] args) {
-        int[] array = {1,2,3};
+        int[] array = {1, 2, 3};
+        print(array);
+    }
+
+    public static void print(int[] array) {
         System.out.println(array);
     }
+
+
 }
